495f89b422116a27b2a5a3b273508dc8d15ebc78
Urse Sabina-Teodora <sabinaurse16@gmail.com>
2025-11-26 12:56:05 +0200

    first

---
+++ Models/DTOs/AIAssistantController/AIAssistantControllerPostMessageResponseDTO.cs
@@ -0,0 +1,6 @@
+namespace HCI.Assistant.Api.Models.DTOs.AIAssistantController;
+
+public class AIAssistantControllerPostMessageResponseDTO
+{
+    public string? TextMessage { get; set; }
+}
---
+++ Models/DTOs/AIAssistantControllerPostMessageRequestDTO.cs
@@ -0,0 +1,6 @@
+namespace HCI.Assistant.Api.Models.DTOs.AIAssistantController;
+
+public class AIAssistantControllerPostMessageRequestDTO
+{
+    public string? TextMessage { get; set; }
+}
